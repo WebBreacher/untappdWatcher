d10409596e139aaf957d959ef599e7e6d43fdc63
WebBreacher <WebBreacher@users.noreply.github.com>
2019-09-03 22:25:29 -0400

    Script working with sqlitedb

--- untappdWatcher.py
+++ untappdWatcher.py
@@ -4,6 +4,8 @@
     Purpose: To look up a bar/pub/brewery on Untappd.com and watch who drinks there
     Inspired by Ryan in my SEC487 class!
     https://github.com/WebBreacher/untappdWatcher
+
+    Huge thanks to https://www.sqlitetutorial.net/sqlite-python/insert/ for the sqlite python help
 """
 
 import argparse
@@ -15,9 +17,6 @@ import sqlite3
 from sqlite3 import Error
 import sys
 
-db_file = 'untappdWatcher.sqlite3'
-
-
 ####
 # Functions
 ####
@@ -28,12 +27,6 @@ parser.add_argument('-b', '--bar', required=True, help='Bar/pub/brewery to resea
 args = parser.parse_args()'''
 
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    From https://www.sqlitetutorial.net/sqlite-python/create-tables/
-    """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -41,41 +34,37 @@ def create_connection(db_file):
     except Error as e:
         print('[!!!] Error! Cannot connect to DB - {}'.format(e))
         sys.exit()
- 
+
     return conn
 
-def create_table():
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    From https://www.sqlitetutorial.net/sqlite-python/create-tables/
-    """
+def create_table(conn, db_file):
     create_table_sql = """CREATE TABLE IF NOT EXISTS watcher (
                                     barname TEXT NOT NULL,
                                     username TEXT NOT NULL,
                                     postdate TEXT NOT NULL);"""
     try:
-        conn = create_connection(db_file)
         c = conn.cursor()
         c.execute(create_table_sql)
     except Error as e:
         print('[!!!] Error! Cannot create table - {}'.format(e))
         sys.exit(1)
 
-def insert_bar_data(bar_data):
-    """
-    Create a new row into the bar_data table
-    :param conn:
-    :param bar_data:
-    :return: watcher id
-    https://www.sqlitetutorial.net/sqlite-python/insert/
-    """
+def insert_bar_data(conn, bar_data):
     sql = ''' INSERT INTO watcher(barname,username,postdate) VALUES(?,?,?) '''
-    conn = create_connection(db_file)
     cur = conn.cursor()
     cur.execute(sql, bar_data)
     return cur.lastrowid
+
+def search_for_bar_data(conn, bar_data):
+    sql = ''' SELECT * FROM watcher WHERE barname=? AND username=? AND postdate=? '''
+    cur = conn.cursor()
+    cur.execute(sql, bar_data)
+    rows = cur.fetchall()
+    if rows:
+        print('[-] Found {}.'.format(rows))
+    else:
+        print("[+] Inserting {}.".format(bar_data))
+        insert_bar_data(conn, bar_data)
 
 def get_data_from_untappd(url):
     # Setting up and Making the Web Call
@@ -85,32 +74,27 @@ def get_data_from_untappd(url):
         # Make web request for that URL and don't verify SSL/TLS certs
         response = requests.get(url, headers=headers, verify=False)
         return response.text
-
     except Exception as e:
         print('[!]   ERROR - Untappd issue: {}'.format(str(e)))
         sys.exit(1)
 
-
-def get_bar_data(passed_bar):
+def get_bar_data(conn, db_file, passed_bar):
     # Parsing bar information
     print("\n[ ] Requesting {}".format(passed_bar))
     resp = get_data_from_untappd(passed_bar)
     html_doc = BeautifulSoup(resp, 'html.parser')
     bar1 = html_doc.find_all('a', 'time timezoner track-click')
     barname = html_doc.title.string.strip(' - Untappd')
-    #print(barname)
 
-    if bar1:
-        for bar in bar1:
-            matchuserobj = re.findall('user/(.+)/checkin/.*?">(.+)</a>', str(bar), re.DOTALL)
-            if matchuserobj:
-                for user in matchuserobj:
-                    place = '{}, {}'.format(user[0],user[1])
-                    #print(place)
-                    bar_data = (barname, user[0], user[1])
-                    print(bar_data)
-                    id = insert_bar_data(bar_data)
-                    print(id)
+    with conn:
+        if bar1:
+            for bar in bar1:
+                matchuserobj = re.findall('user/(.+)/checkin/.*?">(.+)</a>', str(bar), re.DOTALL)
+                if matchuserobj:
+                    for user in matchuserobj:
+                        #place = '{}, {}'.format(user[0],user[1])
+                        bar_data = (barname, user[0], user[1])
+                        search_for_bar_data(conn, bar_data)
 
 ###########################
 # Start
@@ -119,20 +103,15 @@ def get_bar_data(passed_bar):
 # Suppress HTTPS warnings
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+# Set up the connection to the database
+db_file = r'untappdWatcher.sqlite3'
+conn = create_connection(db_file)
+
 # Check for DB table
-create_table()
+create_table(conn, db_file)
 
-
-###############
 # Get bar info
-###############
-#bar = get_bar_data('https://untappd.com/v/81bay-brewing-co/4883588')
-bar = get_bar_data('https://untappd.com/v/ford-s-garage/3103119')
-#bar = get_bar_data('https://untappd.com/v/4-creeks-brewhouse/8829089')
+bar = get_bar_data(conn, db_file, 'https://untappd.com/v/81bay-brewing-co/4883588')
+bar = get_bar_data(conn, db_file, 'https://untappd.com/v/ford-s-garage/3103119')
+bar = get_bar_data(conn, db_file, 'https://untappd.com/v/4-creeks-brewhouse/8829089')
 
-
-# not inserting into the DB
-
-# Check to see if there already is an entry for what we have
-
-# if not, add it to the DB
